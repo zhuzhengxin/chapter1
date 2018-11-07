5dafb81f506b9ba59ae9cbf1ef652ae5fb966a32
zhuzhengxin <406068803@qq.com>
2018-11-07 13:58:47 +0800

    zz

--- service-hi/src/main/java/com/forezp/com/forezp/Hicontroller.java
+++ service-hi/src/main/java/com/forezp/com/forezp/Hicontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dd")
+//@RequestMapping("/dd")
 public class Hicontroller {
 
     @Value("${server.port}")
--- service-zuul/src/main/java/com/forezp/MyFilter.java
+++ service-zuul/src/main/java/com/forezp/MyFilter.java
@@ -14,22 +14,41 @@ import javax.servlet.http.HttpServletRequest;
 public class MyFilter extends ZuulFilter{
 
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
     @Override
+    /*
+     * 定义过滤器类型，决定该规则处理请求的哪个生命周期执行，有以下四个阶段：
+     * pre: 在请求被路由之前执行
+     * route : 在路由请求调用
+     * post : 在route和err之后被调用
+     * error：在处理请求发生错误时调用
+     */
     public String filterType() {
         return "pre";
     }
 
     @Override
+    /**
+     * 过滤器执行顺序,数字越小优先级越高
+     */
     public int filterOrder() {
         return 0;
     }
 
     @Override
+    /**
+     * 判断过滤器是否需要被执行
+     * true 是  false  不执行
+     */
     public boolean shouldFilter() {
-        return true;
+        return false;
     }
 
     @Override
+    /**
+     * 过滤器执行的具体逻辑
+     * 只有URL后面有accToken的才被允许访问
+     */
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
